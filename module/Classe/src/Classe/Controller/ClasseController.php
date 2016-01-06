1317d7e4eae5bf2f95ae777c16e4097567d9ae00
Emmanuel Marti <emmanuel.marti@ynov.com>
2016-01-06 17:09:49 +0100

    fin du module classe

--- module/Classe/src/Classe/Controller/ClasseController.php
+++ module/Classe/src/Classe/Controller/ClasseController.php
@@ -3,6 +3,8 @@
 
  use Zend\Mvc\Controller\AbstractActionController;
  use Zend\View\Model\ViewModel;
+ use Classe\Model\Classe;
+ use Classe\Form\ClasseForm;
 
  class ClasseController extends AbstractActionController{
  	protected $classeTable;
@@ -13,12 +15,92 @@
     }
 
      public function addAction(){
+     	$form = new ClasseForm();
+        $form->get('submit')->setValue('Add');
+
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            $classe = new Classe();
+            $form->setInputFilter($classe->getInputFilter());
+            $form->setData($request->getPost());
+
+            if ($form->isValid()) {
+                $classe->exchangeArray($form->getData());
+                $this->getClasseTable()->saveClasse($classe);
+
+                 // Redirect to list of albums
+                return $this->redirect()->toRoute('classe');
+            }
+        }
+        return array('form' => $form);
     }
 
      public function editAction(){
+
+     	$id = (int) $this->params()->fromRoute('id', 0);
+         if (!$id) {
+             return $this->redirect()->toRoute('classe', array(
+                 'action' => 'add'
+             ));
+         }
+
+         // Get the Album with the specified id.  An exception is thrown
+         // if it cannot be found, in which case go to the index page.
+         try {
+             $classe = $this->getClasseTable()->getClasse($id);
+         }
+         catch (\Exception $ex) {
+             return $this->redirect()->toRoute('classe', array(
+                 'action' => 'index'
+             ));
+         }
+
+         $form  = new ClasseForm();
+         $form->bind($classe);
+         $form->get('submit')->setAttribute('value', 'Edit');
+
+         $request = $this->getRequest();
+         if ($request->isPost()) {
+             $form->setInputFilter($classe->getInputFilter());
+             $form->setData($request->getPost());
+
+             if ($form->isValid()) {
+                 $this->getClasseTable()->saveClasse($classe);
+
+                 // Redirect to list of albums
+                 return $this->redirect()->toRoute('classe');
+             }
+         }
+
+         return array(
+             'id' => $id,
+             'form' => $form,
+         );
     }
 
      public function deleteAction(){
+        $id = (int) $this->params()->fromRoute('id', 0);
+         if (!$id) {
+             return $this->redirect()->toRoute('classe');
+         }
+
+         $request = $this->getRequest();
+         if ($request->isPost()) {
+             $del = $request->getPost('del', 'No');
+
+             if ($del == 'Yes') {
+                 $id = (int) $request->getPost('id');
+                 $this->getClasseTable()->deleteClasse($id);
+             }
+
+             // Redirect to list of albums
+             return $this->redirect()->toRoute('classe');
+         }
+
+         return array(
+             'id'    => $id,
+             'classe' => $this->getClasseTable()->getClasse($id)
+         );
     }
     public function getClasseTable()
     {
