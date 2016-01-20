1807424ef53479634c14100cdd49c63be743f1ff
Kirito <Didou-PC@MacBook-Pro-de-Didou.local>
2016-01-20 15:37:00 +0100

    Modif cour

--- config/application.config.php
+++ config/application.config.php
@@ -8,7 +8,7 @@
 return array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
-        'Application','Album','Classe'
+        'Application','Album','Classe','Cour',
     ),
 
     // These are various options for the listeners attached to the ModuleManager
