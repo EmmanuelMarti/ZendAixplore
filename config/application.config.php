ab53e508de882b312347bb846012ac859e52af86
Kirito <Didou-PC@MacBook-Pro-de-Didou.local>
2016-02-03 16:45:07 +0100

    Ajout ZfcAdmin

--- config/application.config.php
+++ config/application.config.php
@@ -8,7 +8,7 @@
 return array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
-        'Application','Album','Classe','ZfcBase', 'ZfcUser','Topic','Cour'
+        'Application','Album','Classe','ZfcBase', 'ZfcUser','Topic','Cour','ZfcAdmin'
     ),
 
     // These are various options for the listeners attached to the ModuleManager
